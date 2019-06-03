36da758b5ce9a6865497ffdf75e8bce8582947dd
Katya Sazonova <45514862+KatyaSaz@users.noreply.github.com>
2019-06-03 13:23:09 +0300

    work
    working

--- send.php
+++ send.php
@@ -1,40 +1,26 @@
 <?php
 $fio = $_POST['fio'];
-$tel = $_POST['tel'];
+$tel = $_POST['phone'];
 $email = $_POST['email'];
-$message = $_POST['message'];
 $fio = htmlspecialchars($fio);
 $email = htmlspecialchars($email);
 $tel = htmlspecialchars($tel);
-$message = htmlspecialchars($message);
 $fio = urldecode($fio);
 $email = urldecode($email);
 $tel = urldecode($tel);
-$message = urldecode($message);
 $fio = trim($fio);
 $email = trim($email);
 $tel = trim($tel);
-$message = trim($message);
-
 
 $mysqli = new mysqli('localhost', 'root', 'PinkFullOf87', 'register info');
+$dsn="mysql:dbname=register info;host=127.0.0.1:3306";
+$user="root";
+$password="PinkFullOf87";
+$db = new PDO($dsn,$user,$password);
 if (mysqli_connect_errno()) 
 {
    echo "Подключение невозможно: ".mysqli_connect_error();
 }
-
-$query = sprintf("SELECT name, email, telephone number=,%s%s%s",
-    mysql_real_escape_string($fio),
-    mysql_real_escape_string($email),
-	mysql_real_escape_string($tel));
-
-// Выполняем запрос
-$result = mysql_query($query)
-	or die("Invalid query: " . mysql_error());
-
- $mysqli->close();
-
-//if (mail("ekaterina@sazonov-online.info", "Заявка с сайта", "Имя:".$fio.".\r\n E-mail: ".$email.".\r\n Телефон: ".$tel."".$message))
-//} else //{ 
- //  echo "Error";
-
+$query="INSERT INTO `info about users` (`Name`, `Email`, `Number`) VALUES ('{$fio}', '{$email}','{$tel}');";
+$result = $db->query($query);
+ ?>
